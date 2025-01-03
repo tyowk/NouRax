1f968bede4a06f4004a1882d6485467f98d0f96e
tyowk <tyowk@outlook.com>
2025-01-03 02:56:40 +0000

    meow

---
+++ src/commands/client/owner/meow.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        name: 'meow',
+        code: `
+meow <:meow:1323099838232920144>
+$onlyIf[$hasPermsInChannel[$channelId;$clientId;sendmessages]==true;]
+`,
+    },
+];
